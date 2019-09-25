fe80f3bd5815d49fce8f32dad440c812f0547e74
Paul Curtis <pcurtis5688@gmail.com>
2019-09-25 17:08:44 -0400

    Local Repo Commit 9/25/2019

---
+++ content_index.php
@@ -0,0 +1,2 @@
+<img src="resources/img/pcdussitebackgroundsept2019.png"
+     width="1280" height="1024" alt="PaulCurtis.US"/>
--- index.php
+++ index.php
@@ -1,44 +1,30 @@
 <html lang="en">
 <head>
-    <title>Paul Curtis DOT US</title>
+    <title>PaulCurtis.us</title>
     <script src="resources/mdl/material.js" type="application/javascript"></script>
     <link type="text/css" rel="stylesheet" href="resources/mdl/material.css"/>
     <style>
-        .demo-layout-transparent {
-            background: url('/resources/img/pcdussitebackgroundsept2019.png') center / cover;
-            /*background-image: url("/resources/img/pcdus-site-background-sept-2019.png");*/
-        }
-
         .demo-layout-transparent .mdl-layout__header,
         .demo-layout-transparent .mdl-layout__drawer-button {
             /* This background is dark, so we set text to white. Use 87% black instead if
                your background is light. */
-            background-color: #000000;
+            color: #3496e9;
         }
 
         .mdl-layout-title {
-            color: #FFFFFF;
-            background-color: #000000;
+            color: black;
         }
 
         .mdl-layout {
-            background-image: url('../PaulCurtisDotUS/resources/img/pcdussitebackgroundsept2019.png');
+            /*background-image: url('../PaulCurtisDotUS/resources/img/pcdussitebackgroundsept2019.png');*/
+        }
+
+        .mdl-layout__header-row {
+            background-color: purple;
         }
 
         .mdl-layout__drawer {
-            background-color: black;
-        }
-
-        .mdl-navigation {
-            background-color: black;
-        }
-
-        .mdl-layout-spacer {
-            background-color: black;
-        }
-
-        .material-icon {
-            color: white;
+            background-color: purple;
         }
     </style>
 </head>
@@ -47,11 +33,12 @@
     <header class="mdl-layout__header mdl-layout__header--transparent">
         <div class="mdl-layout__header-row">
             <!-- Title -->
-            <span class="mdl-layout-title">Paul Curtis DOT US</span>
+            <span class="mdl-layout-title">PaulCurtis.US</span>
             <!-- Add spacer, to align navigation to the right -->
             <div class="mdl-layout-spacer"></div>
             <!-- Navigation -->
             <nav class="mdl-navigation">
+                <a class="mdl-navigation__link" href="">Home (Kansas, toto)</a>
                 <a class="mdl-navigation__link" href="">Resume</a>
                 <a class="mdl-navigation__link" href="">Portfolio</a>
                 <a class="mdl-navigation__link" href="">Get in touch</a>
@@ -60,18 +47,16 @@
         </div>
     </header>
     <div class="mdl-layout__drawer">
-        <span class="mdl-layout-title">paulcurtis.us</span>
+        <span class="mdl-layout-title">PaulCurtis.US</span>
         <nav class="mdl-navigation">
-            <a class="mdl-navigation__link" href="index.php">
-                <img src="resources/icons/home-18/1x/baseline_home_black_18dp.png" alt="Home"/>
-                Kansas</a>
-            <a class="mdl-navigation__link" href="resume.php">Resume</a>
+            <a class="mdl-navigation__link" href="">Resume</a>
             <a class="mdl-navigation__link" href="">Portfolio</a>
             <a class="mdl-navigation__link" href="">Get in touch</a>
             <a class="mdl-navigation__link" href="">Other Stuff</a>
         </nav>
     </div>
     <main class="mdl-layout__content">
+        <?php include('content_index.php'); ?>
     </main>
 </div>
 </body>
