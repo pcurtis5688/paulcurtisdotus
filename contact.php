654a1019124024d43eaad5f34920158a2ffa3713
Paul Curtis <pcurtis5688@gmail.com>
2019-11-13 00:07:07 -0500

    11-13-2019 Adjusments

--- contact.php
+++ contact.php
@@ -6,6 +6,9 @@
 <link rel="stylesheet" type="text/css"
 	href="./resources/style/pcdusnov2019style.css" />
 <script type="text/javascript" src="./resources/mdl/material.min.js"></script>
+<!--Let browser know website is optimized for mobile-->
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
 <style>
 .mdl-layout__content {
 	padding: 5%;
@@ -43,8 +46,9 @@ main .content_body {
 			<p class="content_body">
 				Email: <a href="mailto:pcurtis5688@gmail.com">pcurtis5688@gmail.com</a>
 				<br> <br>Mobile: <a href="telephone:4127160747">{+1}412-716-0747</a>
-				<br> <br> LinkedIn: <a href="https://linkedin.com/in/paulcphilly/">linkedin.com/in/paulcphilly/
-				</a> <a href="https://linkedin.com/in/paulcphilly/" target="_blank"><img
+				<br> <br> LinkedIn: <a href="https://linkedin.com/in/paulcphilly/"
+					target="_blank">linkedin.com/in/paulcphilly/ </a> <a
+					href="https://linkedin.com/in/paulcphilly/" target="_blank"><img
 					alt="Paul's LinkedIn" src="./resources/img/pcdus-li-icon-sm.png"
 					style="width: 42px; height: 42px; margin-left: 15px;" /></a>
 			</p>
--- pcdus_nav_header.php
+++ pcdus_nav_header.php
@@ -15,7 +15,7 @@
 }
 
 .mdl-layout__drawer {
-	background-color: black;
+	background-color: #000000;
 }
 
 .mdl-navigation .mdl-navigation__link {
@@ -59,48 +59,55 @@
 }
 </style>
 </head>
-<div class="demo-layout-transparent mdl-layout mdl-js-layout">
-	<header class="mdl-layout__header mdl-layout__header--transparent">
-		<div class="mdl-layout__header-row">
-			<a href="index.php"> <img alt="paulcurtis.us:logo"
-				src="./resources/img/pcdussitebackgroundsept2019.png" /></a>
-			<!-- Navigation -->
-			<nav class="mdl-navigation">
-				<a class="mdl-navigation__link" href="index.php"
-					style="font-size: 14pt;"> <i class="material-icons">home</i>Home
-				</a> <a class="mdl-navigation__link" href="portfolioresume.php"
-					style="font-size: 14pt;"><i class="material-icons">assignment</i>Portfolio</a><a
-					class="mdl-navigation__link" href="https://github.com/pcurtis5688"
-					target="_blank"> <img alt="GitHub Repo"
-					src="./resources/img/PCDUS-GitHub-Icon-Light64.png" height="42px"
-					width="42px" />
-				</a> <a class="mdl-navigation__link" href="contact.php"> <i
-					class="material-icons">mail</i></a>
-			</nav>
-			<!-- Add spacer to align navigation to the right -->
-			<div class="mdl-layout-spacer"></div>
+<body>
+	<div id="logodiv"
+		style="background-color: black; width: 100%; text-align: center;">
+		<a href="index.php"><img alt="paulcurtis.us:logo"
+			src="./resources/img/pcdussitebackgroundsept2019.png" /></a>
+		<div class="demo-layout-transparent mdl-layout mdl-js-layout">
+			<header class="mdl-layout__header mdl-layout__header--transparent">
+				<div class="mdl-layout__header-row">
+					<!-- Navigation -->
+					<nav class="mdl-navigation">
+						<a class="mdl-navigation__link" href="index.php"
+							style="font-size: 14pt;"> <i class="material-icons">home</i>Home
+						</a> <a class="mdl-navigation__link" href="portfolioresume.php"
+							style="font-size: 14pt;"><i class="material-icons">assignment</i>Portfolio</a>
+						<a class="mdl-navigation__link"
+							href="https://github.com/pcurtis5688" target="_blank"> <img
+							alt="GitHub Repo"
+							src="./resources/img/PCDUS-GitHub-Icon-Light64.png" height="42px"
+							width="42px" />
+						</a> <a target="_blank" class="mdl-navigation__link"
+							href="https://linkedin.com/in/paulcphilly/"><img
+							alt="Paul C's LinkedIn" width="42px" height="36px"
+							src="./resources/img/pcdus-li-icon-sm.png"></img></a> <a
+							class="mdl-navigation__link" href="contact.php"> <i
+							class="material-icons">mail</i>
+						</a>
+					</nav>
+					<!-- Add spacer to align navigation to the right -->
+					<div class="mdl-layout-spacer"></div>
+				</div>
+			</header>
+			<div class="mdl-layout__drawer">
+				<nav class="mdl-navigation">
+					<img alt="paulcurtis.us" style="width: 100%;"
+						src="./resources/img/pcdussitebackgroundsept2019.png" /> <a
+						class="mdl-navigation__link" href="index.php"> <i
+						class="material-icons">home</i> Home
+					</a> <a class="mdl-navigation__link" href="portfolioresume.php"> <i
+						class="material-icons">assignment</i>Portfolio/Resume
+					</a><a class="mdl-navigation__link"
+						href="https://github.com/pcurtis5688" target="_blank"> <img
+						alt="https://github.com/pcurtis5688/"
+						src="./resources/img/PCDUS-GitHub-Icon-Light64.png" height="36px"
+						width="36px" /> @pcurtis5688<br>
+					</a><a class="mdl-navigation__link" href="contact.php"> <i
+						class="material-icons">mail</i> Contact Me
+					</a>
+				</nav>
+			</div>
 		</div>
-	</header>
-	<div class="mdl-layout__drawer">
-		<img src="./resources/img/pcdussitebackgroundsept2019.png"
-			alt="PaulCurtis.US" />
-		<nav class="mdl-navigation">
-			<a class="mdl-navigation__link" href="index.php"> <i
-				class="material-icons">home</i> Home
-			</a> <a class="mdl-navigation__link" href="portfolioresume.php"> <i
-				class="material-icons">assignment</i>Portfolio/Resume
-			</a><a class="mdl-navigation__link"
-				href="https://github.com/pcurtis5688" target="_blank"> <img
-				alt="https://github.com/pcurtis5688/"
-				src="./resources/img/PCDUS-GitHub-Icon-Light64.png" height="36px"
-				width="36px" /> @pcurtis5688<br>
-			</a> <a class="mdl-navigation__link"
-				href="https://linkedin.com/in/paulcphilly/" target="_blank"><img
-				alt="LinkedIn" src="./resources/img/pcdus-li-icon-sm.png"
-				height="42px" width="42px"></img>&nbsp;LinkedIn </a><a
-				class="mdl-navigation__link" href="contact.php"> <i
-				class="material-icons">mail</i> Contact Me
-			</a>
-		</nav>
-	</div>
-</div>
+
+</body>
