b17c6e30124014220e4e36ba52a44983b8bed17c
Paul Curtis <pcurtis5688@gmail.com>
2020-03-19 14:28:40 -0400

    v0.79

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <meta name="viewport"
 	content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta http-equiv="x-ua-compatible" content="ie=edge">
-<title>|| PaulCurtis.US ~ Home ||</title>
+<title>|| PaulCurtis.US - Home ||</title>
 <!-- Font Awesome -->
 <link rel="stylesheet"
 	href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
@@ -16,218 +16,25 @@
 <!-- Your custom styles (optional) -->
 <link href="resources/css/style.min.css" rel="stylesheet">
 <link href="resources/css/pcdus-style.css" rel="stylesheet">
-<style type="text/css">
-html, body, header, .view {
-	height: 100%;
-}
-
-@media ( max-width : 740px) {
-	html, body, header, .view {
-		height: 1000px;
-	}
-}
-
-@media ( min-width : 800px) and (max-width: 850px) {
-	html, body, header, .view {
-		height: 650px;
-	}
-}
-
-@media ( min-width : 800px) and (max-width: 850px) {
-	.navbar
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	:not
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	(
-	.top-nav-collapse
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	)
-	{
-	background
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	:
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	#1C2331
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	!
-	important
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	;
-}
-}
-</style>
 </head>
+
 <!-- Navbar -->
 <nav
 	class="navbar fixed-top navbar-expand-lg navbar-dark scrolling-navbar">
 	<div class="container">
-
-		<!-- Brand -->
-		<a class="navbar-brand" href="index.php"> <strong>Paul Curtis</strong>
+		<a class="navbar-brand" href="index.php"> <strong>PaulCurtis.us</strong>
 		</a>
 
 		<!-- Collapse -->
 		<button class="navbar-toggler" type="button" data-toggle="collapse"
 			data-target="#navbarSupportedContent"
-			aria-controls="navbarSupportedContent" aria-expanded="false"
+			aria-controls="navbarSupportedContent" aria-expanded="true"
 			aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
 
 		<!-- Links -->
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
-
 			<!-- Left -->
 			<ul class="navbar-nav mr-auto">
 				<li class="nav-item active"><a class="nav-link" href="#">Home <span
@@ -236,8 +43,8 @@ html, body, header, .view {
 				<li class="nav-item"><a class="nav-link" href="#"
 					onclick="changeTitle();">Contact Information</a></li>
 				<li class="nav-item"><a class="nav-link"
-					href="resources/docs/Paul_Curtis_Resume_March_2020_dev.pdf"
-					target="_blank">View & Download CV / Resume</a></li>
+					href="resources/docs/ResumePaulCurtisMarch2020_INDEED.pdf"
+					target="_blank">My CV / Resume</a></li>
 				<li class="nav-item"><a class="nav-link"
 					href="https://mdbootstrap.com/docs/jquery/" target="_blank">Portfolio</a></li>
 			</ul>
@@ -257,40 +64,32 @@ html, body, header, .view {
 		</div>
 	</div>
 </nav>
-<!-- Navbar -->
 
-<!-- Full Page Intro -->
+<!-- PCDUS Home Page Contents -->
 <div class="view full-page-intro"
 	style="background-image: url('https://mdbootstrap.com/img/Photos/Others/images/78.jpg'); background-repeat: no-repeat; background-size: cover;">
-
 	<!-- Mask & flexbox options-->
 	<div
 		class="mask rgba-black-light d-flex justify-content-center align-items-center">
-
 		<!-- Content -->
-		<div class="container">
-
+		<div class="container" style="margin-top: 100px;">
 			<!--Grid row-->
 			<div class="row wow fadeIn">
-
 				<!--Grid column-->
 				<div class="col-md-6 mb-4 white-text text-center text-md-left">
-					<h1 class="display-4 font-weight-bold">Home</h1>
+					<h1 class="display-5 font-weight-bold textalign">19 March 2020</h1>
 					<hr class="hr-light">
-					<p>
-						<strong>Welcome to my online presence...</strong>
-					</p>
 					<p class="mb-4 d-none d-md-block">
-						<strong>I might start a blog here, who knows!</strong>
+						<strong>Greetings and welcome to my realm! This site has been a
+							long time coming primarily because technology changes quickly and
+							I found myself too busy to publish a production version. That
+							being said, I'm hoping to keep this up-to-date with some of my
+							current endeavors and I encourage anyone with any development
+							opportunities to reach out to me. I'm available any time at my
+							email (pcurtis5688@gmail.com) or at my cell (412-716-0747) in the
+							contact info. Thanks for reading and have a great day! - Paul </strong>
 					</p>
-					<!-- 					<a target="_blank" -->
-					<!-- 						href="https://mdbootstrap.com/education/bootstrap/" -->
-					<!-- 						class="btn btn-indigo btn-lg">Start free tutorial <i -->
-					<!-- 						class="fas fa-graduation-cap ml-2"></i> -->
-					</a>
 				</div>
-				<!--Grid column-->
-
 				<!--Grid column-->
 				<div class="col-md-6 col-xl-5 mb-4">
 					<!--Card-->
@@ -344,75 +143,19 @@ html, body, header, .view {
 	</div>
 	<!-- Mask & flexbox options-->
 </div>
-<!-- Full Page Intro -->
 
-<!--Main layout-->
-<!-- <main> <div class="container" style="margin-top:8px;">-->
-<!-- 		<section> -->
-<!-- 			<h3 class="h3 text-center mb-5">About MDB</h3> -->
-<!-- 			<div class="row wow fadeIn"> -->
-<!-- 				<div class="col-lg-6 col-md-12 px-4"> -->
-<!-- 					<div class="row"> -->
-<!-- 						<div class="col-1 mr-3"> -->
-<!-- 							<i class="fas fa-code fa-2x indigo-text"></i> -->
-<!-- 						</div> -->
-<!-- 						<div class="col-10"> -->
-<!-- 							<h5 class="feature-title">Bootstrap 4</h5> -->
-<!-- 							<p class="grey-text">Thanks to MDB you can take advantage of all -->
-<!-- 								feature of newest Bootstrap 4.</p> -->
-<!-- 						</div> -->
-<!-- 					</div> -->
-<!-- 		<div style="height: 30px"></div>			<div class="row"> -->
-<!-- 						<div class="col-1 mr-3"> -->
-<!-- 							<i class="fas fa-book fa-2x blue-text"></i> -->
-<!-- 						</div> -->
-<!-- 						<div class="col-10"> -->
-<!-- 							<h5 class="feature-title">Detailed documentation</h5> -->
-<!-- 							<p class="grey-text">We give you detailed user-friendly -->
-<!-- 								documentation at your disposal. It will help you to implement -->
-<!-- 								your ideas easily.</p> -->
-<!-- 						</div> -->
-<!-- 					</div> -->
-<!-- 			<div style="height: 30px"></div>		<div class="row"> -->
-<!-- 						<div class="col-1 mr-3"> -->
-<!-- 							<i class="fas fa-graduation-cap fa-2x cyan-text"></i> -->
-<!-- 						</div> -->
-<!-- 						<div class="col-10"> -->
-<!-- 							<h5 class="feature-title">Lots of tutorials</h5> -->
-<!-- 							<p class="grey-text">We care about the development of our users. -->
-<!-- 								We have prepared numerous tutorials, which allow you to learn -->
-<!-- 								how to use MDB as well as other technologies.</p> -->
-<!-- 						</div> -->
-<!-- 					</div> -->
-<!-- 				</div> -->
-<!-- 				<div class="col-lg-6 col-md-12"> -->
-<!-- 					<p class="h5 text-center mb-4">Watch our "5 min Quick Start" -->
-<!-- 						tutorial</p> -->
-<!-- 					<div class="embed-responsive embed-responsive-16by9"> -->
-<!-- 						<iframe class="embed-responsive-item" -->
-<!-- 							src="https://www.youtube.com/embed/cXTThxoywNQ" allowfullscreen></iframe> -->
-<!-- 					</div> -->
-<!-- 				</div> -->
-<!-- 			</div> -->
-<!-- 		</section> -->
-<!-- 	</div> -->
-<!-- </main> -->
-<!--Main layout-->
-
-<!--Footer-->
+<!------------------FOOTER ----------------------------->
 <footer class="page-footer text-center font-small mt-4 wow fadeIn">
 	<!--Call to action-->
 	<div class="pt-4">
 		<a class="btn btn-outline-white"
-			href="resources/docs/Paul_Curtis_Resume_March_2020_dev.pdf"
+			href="resources/docs/ResumePaulCurtisMarch2020_INDEED.pdf"
 			target="_blank" role="button">View or Download CV / Resume<i
 			class="fas fa-download ml-2"></i>
 		</a>
 	</div>
 	<!--/.Call to action-->
-
 	<hr class="my-4">
-
 	<!-- Social icons -->
 	<div class="pb-4">
 		<a href="https://github.com/pcurtis5688" target="_blank"> <img
@@ -427,16 +170,11 @@ html, body, header, .view {
 </footer>
 <!--/.Footer-->
 
-<!-- SCRIPTS -->
-<!-- JQuery -->
+<!-- ----------------------SCRIPTS---------------------------------------------------- -->
 <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
-<!-- Bootstrap tooltips -->
 <script type="text/javascript" src="js/popper.min.js"></script>
-<!-- Bootstrap core JavaScript -->
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
-<!-- MDB core JavaScript -->
 <script type="text/javascript" src="js/mdb.min.js"></script>
-<!-- Initializations -->
 <script type="text/javascript">
     // Animations initialization
     new WOW().init();
