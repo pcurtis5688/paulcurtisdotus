2ad525c29c4f8c617b8ad5b5ea267bb38a087abf
Paul Curtis <pcurtis5688@gmail.com>
2020-03-23 01:01:50 -0400

    v0.98 23-March-2020

--- content/contactcontent.php
+++ content/contactcontent.php
@@ -1,7 +1,11 @@
-<div style="text-align: left;">
-	<h1 style="padding-top: 50px;">Paul Curtis</h1>
-	<h2>paul@paulcurtis.us</h2>
-	<h2>pcurtis5688@gmail.com</h2>
+<div style="text-align: center;">
+	<h1 style="padding-top: 1em;">Paul Curtis</h1>
+	<h2>
+		<a href="mailto:pcurtis5688@gmail.com">pcurtis5688@gmail.com</a>
+	</h2>
+	<h2>
+		<a href="mailto:paul@paulcurtis.us@gmail.com">paul@paulcurtis.us</a>
+	</h2>
 	<h2>
 		<a href="tel:412-716-0747">412-716-0747</a>
 	</h2>
