c4f878017b7b4fe1996b74aa63b27836c659cd83
Paul Curtis <pcurtis5688@gmail.com>
2020-04-12 08:26:10 -0400

    12 apr 2020 updates to contact form so at least aesthetic

--- contactform.php
+++ contactform.php
@@ -3,31 +3,27 @@
 <head>
 </head>
 <body>
-	<div class="container">
+	<div class="container" style="padding-bottom: 200px;">
 		<div class="row">
 			<div class="col-xl-8 offset-xl-2 py-5">
-				<h1>Contact Form</h1>
+				<h1>Contact Me</h1>
 				<form id="contact-form" method="post" action="sendemail.php"
 					role="form">
 					<div class="messages"></div>
 					<div class="controls">
 						<div class="row">
-							<div class="col-md-6">
-								<div class="form-group" style="width: 200%;">
+							<div class="col-md-12">
+								<div class="form-group">
 									<input id="contact_form" type="text" name="first_name"
 										class="form-control" placeholder="First Name"
-										required="required" data-error="First Name is required."><input
-										id="contact_form" type="text" name="last_name"
+										required="required" data-error="First Name is required."> <br>
+									<input id="contact_form" type="text" name="last_name"
 										class="form-control" placeholder="Last Name"
-										required="required" data-error="Last Name is required.">
+										required="required" data-error="Last Name is required."><input
+										type="submit" class="btn btn-success btn-send"
+										value="Send Email">
 									<div class="help-block with-errors"></div>
 								</div>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-md-12">
-								<input type="submit" class="btn btn-success btn-send"
-									value="Send Email">
 							</div>
 						</div>
 					</div>
