dcd88c29585a055ddcde0efd39a12e0fa2afc697
Paul Curtis <pcurtis5688@gmail.com>
2020-05-05 10:52:26 -0400

    v1.20111 dbconn is working!

---
+++ scripts/paulcurtisdotusdbconn.php
@@ -0,0 +1,53 @@
+<?php
+$pcdusdbHost = "localhost";
+$pcdusdbUserName = "root";
+$pcdusdbPass = "";
+$paulcurtisdotusdb = "paulcurtisdotusdb";
+$dbCon = mysqli_connect($pcdusdbHost, $pcdusdbUserName, $pcdusdbPass, $paulcurtisdotusdb);
+if (! $dbCon) {
+    echo "Conenction Fail";
+}
+mysqli_select_db($dbCon, $paulcurtisdotusdb);
+$qry = "SELECT * FROM blog_entry;";
+$Response = mysqli_query($dbCon, $qry);
+
+$row = mysqli_fetch_row($Response);
+var_dump($row);
+?>
+// /* Attempt MySQL server connection. Assuming you are running MySQL
+//  server with default setting (user 'root' with no password) */
+// $link = mysqli_connect("localhost", "root", "", "");
+
+// // Check connection
+// if($link === false){
+//     die("ERROR: Could not connect. " . mysqli_connect_error());
+// }
+
+// // Attempt select query execution
+// $sql = "SELECT * FROM blog_entry";
+// if($result = mysqli_query($link, $sql)){
+//     if(mysqli_num_rows($result) > 0){
+//         echo "
+<table>
+	"; // echo "
+	<tr>
+		"; // echo "
+		<th>id</th>"; // echo "
+		<th>first_name</th>"; // echo "
+		<th>last_name</th>"; // echo "
+		<th>email</th>"; // echo "
+	</tr>
+	"; // while($row = mysqli_fetch_array($result)){ // echo "
+	<tr>
+		"; // echo "
+		<td>" . $row['id'] . "</td>"; // echo "
+		<td>" . $row['first_name'] . "</td>"; // echo "
+		<td>" . $row['last_name'] . "</td>"; // echo "
+		<td>" . $row['email'] . "</td>"; // echo "
+	</tr>
+	"; // } // echo "
+</table>
+"; // // Free result set // mysqli_free_result($result); // } else{ //
+echo "No records matching your query were found."; // } // } else{ //
+echo "ERROR: Could not able to execute $sql. " . mysqli_error($link); //
+} // // Close connection // mysqli_close($link); // ?>
