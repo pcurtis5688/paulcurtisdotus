9edc91f5863ea00424261827aa35da0c219f7c74
Paul Curtis <pcurtis5688@gmail.com>
2020-05-09 14:41:48 -0400

    v2020-5-9-B working contact form now! whoopieee. and i cen elaborte on

--- scripts/sendemail.php
+++ scripts/sendemail.php
@@ -1,7 +1,16 @@
-<html>
-<body><?php 
-// TODO: complete this too...
-echo 'Thanks for reaching out! We will be in touch shortly...';
+<?php
+// TODO: Send Email then Redirect (& Show Message, if possible)...z
+$visitor_first_name = $_POST ['first_name'];
+$visitor_last_name = $_POST ['last_name'];
+$visitor_email_re_address = $_POST ['email_re'];
+$visitor_message = $_POST ['visitor_msg'];
+
+$to = 'paul@paulcurtis.us';
+$subject = 'Message Sent Via PaulCurtis.us Contact Form';
+$message = 'Email: ' . $visitor_email_re_address . ' (' . $visitor_first_name . ' ' . $visitor_last_name . ') - Message: ' . $visitor_message;
+$headers = 'From: ' . $visitor_email_re_address . '\r\n' . 'Reply-To: ' . $visitor_email_re_address . '\r\n' . 'X-Mailer: PHP/' . phpversion ();
+mail ( $to, $subject, $message, $headers );
+$message_sent_alert = 'Message Sent Successfully. Have a wonderful day.';
+echo "<script type='text/javascript'>alert('$message_sent_alert');</script>";
+header('Location: https://www.paulcurtis.us/');
 ?>
-</body>
-</html>
--- web/contactform.php
+++ web/contactform.php
@@ -10,7 +10,7 @@
 		<button type="button" class="btn btn-secondary rounded"
 			style="color: gold;">[Dial Office: 484-442-0052]</button>
 	</a> <br> <br>
-	<form id="contact-form" method="post" action="../scripts/sendemail.php"
+	<form id="contact-form" method="post" action="./scripts/sendemail.php"
 		role="form">
 		<div class="messages"></div>
 		<div class="controls">
@@ -27,11 +27,11 @@
 							style="margin-bottom: 10px;" id="contact_form" type="text"
 							name="email_re" class="form-control" placeholder="[Your Email]"
 							required="required" data-error="Last Name is required.">
-						<textarea cols="125" rows="5"
+						<textarea cols="125" rows="5" id="contact_form" name="visitor_msg"
 							placeholder="Your message goes right here..."
 							data-error="Message is required." />
 						<br> <input type="submit" class="btn btn-primary btn-send"
-							value="Send Email"><br>
+							value="Send Message"><br>
 						<div class="help-block with-errors"></div>
 					</div>
 				</div>
