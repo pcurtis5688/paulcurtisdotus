cea6dcc142a4161ad99b0893a36989b63ae3fa88
Paul Curtis <pcurtis5688@gmail.com>
2020-05-11 21:52:52 -0400

    v2020-05-11-E began using phpstorm again; testing vcs

---
+++ viewfragments/pcdus_footer.php
@@ -0,0 +1,20 @@
+<!-- FOOTER PHP FRAGMENT -->
+<footer class='page-footer text-center font-small wow fadeIn'>
+    <div>
+        <a class='btn btn-outline-white'
+           href='resources/docs/Paul_Curtis_Resume_2020.docx' target='_blank'
+           role='button'>Download CV / Resume<i class='fas fa-download ml-2'></i>
+        </a>
+    </div>
+    <div class='pb-4'>
+        <a href='https://linkedin.com/in/paulcphilly/' target='_blank'> <img
+                    alt='LI' src='resources/img/pcdus-li-icon-sm.png' height='64px'
+                    width='64px'/>
+        </a> <a href='https://github.com/pcurtis5688' target='_blank'> <img
+                    alt='Git' src='resources/img/GitHub-Mark-64px.png'/>
+        </a>
+    </div>
+    <div class='footer-copyright py-3'>Paul Curtis || pcurtis5688@gmail.com
+        || 412-716-0747 || 484-442-0052
+    </div>
+</footer>
